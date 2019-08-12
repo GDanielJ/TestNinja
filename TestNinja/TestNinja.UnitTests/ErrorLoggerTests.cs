3103d7ae1ee630b00fe89363682876cdd35893b7
Daniel Johansson <johansson.gd@gmail.com>
2019-08-12 20:06:00 +0200

    Klar med 3.34.

--- TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Linq;
 using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
     [TestFixture]
-    class ErrorLoggerTests
+    public class ErrorLoggerTests
     {
         [Test]
         public void Log_WhenCalled_SetTheLastErrorProperty()
@@ -16,6 +15,30 @@ namespace TestNinja.UnitTests
             logger.Log("a");
 
             Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args;  };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
         }
     }
 }
