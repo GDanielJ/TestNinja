3cf4697db2d05fbbd5cec0c22e341b40ef343f08
Daniel Johansson <johansson.gd@gmail.com>
2019-08-14 20:36:31 +0200

    Klar med Section 4.

---
+++ TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_ArgumentOutOfRange_ThrowArgumentOutOfRangeException(int speed)
+        {
+            var calculate = new DemeritPointsCalculator();
+
+            Assert.That(() => calculate.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(50, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedResult)
+        {
+            var calculate = new DemeritPointsCalculator();
+
+            var result = calculate.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Push_ArgIsNull_ThrowArgumentNullException()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ArgIsValid_AddObjToStack()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            stack.Push("a");
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_NoObjInStack_ReturnZero()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            var result = stack.Count;
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Count_OneObjInStack_ReturnOne()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            stack.Push("a");
+            var result = stack.Count;
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Pop_NoObjInStack_ThrowInvalidOperationException()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObj_ReturnObjOnTopOfStack()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            var result = stack.Pop();
+
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObj_ObjIsRemoved()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            stack.Pop();
+
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek_NoObjInStack_ThrowInvalidOperationException()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithAFewObj_ReturnObjOnTopOfStack()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            Assert.That(() => stack.Peek(), Is.EqualTo("c"));
+        }
+    }
+}
