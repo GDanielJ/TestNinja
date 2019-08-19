584b1791f787b859fe97a269e6c394c5af389949
Daniel Johansson <johansson.gd@gmail.com>
2019-08-19 20:51:12 +0200

    Klar med 5.55.

--- TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -5,18 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using TestNinja.Mocking;
 using NUnit.Framework;
+using Moq;
 
 namespace TestNinja.UnitTests.Mocking
 {
     [TestFixture]
     public class VideoServiceTests
     {
+        private VideoService _videoService;
+        private Mock<IFileReader> _fileReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnErrorMessage()
         {
-            var service = new VideoService(new FakeFileReader());
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
 
-            var result = service.ReadVideoTitle();
+            var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
