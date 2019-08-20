b8dfc27f94818cfedcceee5ad46dfab24f85fa13
Daniel Johansson <johansson.gd@gmail.com>
2019-08-20 21:25:07 +0200

    Klar med 6.61.

--- TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -14,12 +14,14 @@ namespace TestNinja.UnitTests.Mocking
     {
         private VideoService _videoService;
         private Mock<IFileReader> _fileReader;
+        private Mock<IVideoRepository> _repository;
 
         [SetUp]
         public void SetUp()
         {
             _fileReader = new Mock<IFileReader>();
-            _videoService = new VideoService(_fileReader.Object);
+            _repository = new Mock<IVideoRepository>();
+            _videoService = new VideoService(_fileReader.Object, _repository.Object);
         }
 
         [Test]
@@ -30,6 +32,32 @@ namespace TestNinja.UnitTests.Mocking
             var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AllVideosAreProcessed_ReturnEmptyString()
+        {
+            _repository.Setup(r => r.GetUnprocessedVideos()).Returns(new List<Video>());
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AFewUnprocessedVideos_ReturnStringWithIdsOfUnprocessedVideos()
+        {
+            _repository
+                .Setup(r => r.GetUnprocessedVideos())
+                .Returns(new List<Video> {
+                    new Video {Id = 1, Title = "a", IsProcessed = false},
+                    new Video {Id = 2, Title = "b", IsProcessed = false},
+                    new Video {Id = 3, Title = "c", IsProcessed = false}
+                });
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo("1,2,3"));
         }
     }
 }
