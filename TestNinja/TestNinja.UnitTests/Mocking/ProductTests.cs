da5a6ff2961a17a84068a5996a7bca0b0af3e61d
Daniel Johansson <johansson.gd@gmail.com>
2019-08-20 20:27:56 +0200

    Klar med hela 5.

---
+++ TestNinja/TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+
+    // I kursen skapades här ett exempel på Mock abuse. Struntar i det själv.
+}
