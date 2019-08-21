0109e82fc35ce9ca32ad7ed8d63412ac6cb5356c
Daniel Johansson <johansson.gd@gmail.com>
2019-08-21 20:36:35 +0200

    Klar med 6.64.

---
+++ TestNinja/TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+using Moq;
+using NUnit.Framework;
+using System.Net;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            _fileDownloader
+                .Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadComplete_ReturnTrue()
+        {
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
